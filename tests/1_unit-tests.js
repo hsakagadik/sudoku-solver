b360356310fd52ed17f42cd8385c3a1ee5d706cf
Nayla <naylataguada@gmail.com>
2021-09-24 19:59:08 +0200

    init

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,5 +5,47 @@ const Solver = require('../controllers/sudoku-solver.js');
 let solver;
 
 suite('UnitTests', () => {
+    test('Logic handles a valid puzzle string of 81 characters', function(){
+        assert.fail();
+    });
 
+    test('Logic handles a puzzle string with invalid characters (not 1-9 or .)', function(){
+        assert.fail();
+    });
+
+    test('Logic handles a puzzle string that is not 81 characters in length', function(){
+        assert.fail();
+    });
+
+    test('Logic handles a valid row placement', function(){
+        assert.fail();
+    });
+
+    test('Logic handles a valid column placement', function(){
+        assert.fail();
+    });
+
+    test('Logic handles an invalid column placement', function(){
+        assert.fail();
+    });
+
+    test('Logic handles a valid region (3x3 grid) placement', function(){
+        assert.fail();
+    });
+
+    test('Logic handles an invalid region (3x3 grid) placement', function(){
+        assert.fail();
+    });
+
+    test('Valid puzzle strings pass the solver', function(){
+        assert.fail();
+    });
+
+    test('Invalid puzzle strings fail the solver', function(){
+        assert.fail();
+    });
+    
+    test('Solver returns the expected solution for an incomplete puzzle', function(){
+        assert.fail();
+    });
 });
